459fc6b20fc59f972f054760c1a962fca6de00da
yusuf_sargin <yusuf.sargin@sebit.com.tr>
2021-11-09 13:44:01 +0300

    initialize

---
+++ packages/app2/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/index",
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "../build/app2"),
+    },
+    port: 3001,
+  },
+  output: {
+    // public path can be what it normally is, not a absolute, hardcoded url
+    path: path.resolve(__dirname, "../build/app2"),
+    filename: "[name].[contenthash].js",
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app2",
+      library: { type: "var", name: "app2" },
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/Button",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      excludeChunks: ["app2"],
+    }),
+  ],
+};
